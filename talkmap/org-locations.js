3319a53ae50b57600493d0da3705af450a3f9104
Yi-Chun Hung <lengentyh@gmail.com>
2025-08-03 20:06:25 -0400

    update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,65 @@
-var addressPoints = [
-  [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
-  ],
-  [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
-  ],
-  [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
-  ],
-  [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
-  ]
-];
+var talkLocations = [
+  {
+    title: "Moiré Engineering for Tunable Numbers of Flat Bands and Its Quantum Geometry",
+    venue: "Institute of Physics, Academia Sinica",
+    location: "Taipei, Taiwan",
+    date: "2025-02-13",
+    type: "Invited Talk",
+    lat: 25.0330,
+    lng: 121.5654
+  },
+  {
+    title: "Moiré Engineering for Tunable Numbers of Flat Bands and Its Quantum Geometry",
+    venue: "Department of Physics, National Cheng Kung University",
+    location: "Tainan, Taiwan",
+    date: "2025-02-19",
+    type: "Invited Talk",
+    lat: 22.9997,
+    lng: 120.2270
+  },
+  {
+    title: "Emergent Majorana Kramers pairs in interacting spin/mirror Chern insulators",
+    venue: "APS March Meeting",
+    location: "Anaheim, CA",
+    date: "2025-03-21",
+    type: "Contributing Talk",
+    lat: 33.8366,
+    lng: -117.9143
+  },
+  {
+    title: "Novel Moiré Pattern of a New Two-Dimensional Lattice",
+    venue: "APS March Meeting",
+    location: "Minneapolis, MN",
+    date: "2024-03-08",
+    type: "Contributing Talk",
+    lat: 44.9778,
+    lng: -93.2650
+  },
+  {
+    title: "Time-Reversal Soliton Pair in Two-Dimensional Topological Insulating Systems",
+    venue: "APS March Meeting",
+    location: "Las Vegas, NV",
+    date: "2023-03-08",
+    type: "Contributing Talk",
+    lat: 36.1699,
+    lng: -115.1398
+  },
+  {
+    title: "Edge Mass-Kink and Fractional Corner Charge in Higher-Order Topological Insulators",
+    venue: "1-day workshop on Wannier-functions based Hamiltonians 2022, National Center for Theoretical Sciences, Physics Division",
+    location: "Taipei, Taiwan",
+    date: "2022-04-29",
+    type: "Invited Talk",
+    lat: 25.0330,
+    lng: 121.5654
+  },
+  {
+    title: "Quantum Materials and Topology: Toward Robust Platforms for Quantum Computation",
+    venue: "Annual Meeting of NSF-ExpandQISE 2: Exotic Physics and Applications of Solid-State Qubits",
+    location: "Arlington, VA",
+    date: "2025-07-14",
+    type: "Invited Talk",
+    lat: 38.8816,
+    lng: -77.0910
+  }
+]; 
